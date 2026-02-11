c6b7a527bbc78e0b9d07f677145519a067a9c884
Syed Moinuddin <syedmoinuddin3201@gmail.com>
2026-02-11 13:27:44 +0530

    Create 02_positive_negative_zero.py

---
+++ 02_positive_negative_zero.py
@@ -0,0 +1,10 @@
+# Program: Check if a number is Positive, Negative, or Zero
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
