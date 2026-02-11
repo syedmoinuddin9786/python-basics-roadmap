cea53ae8813b6e7a87b8fd5a547e060ecb16300e
Syed Moinuddin <syedmoinuddin3201@gmail.com>
2026-02-11 13:35:04 +0530

    Create 07_lists_basics.py

---
+++ 07_lists_basics.py
@@ -0,0 +1,13 @@
+# Program: Working with lists
+
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+numbers.append(60)
+print("After adding:", numbers)
+
+numbers.remove(30)
+print("After removing:", numbers)
